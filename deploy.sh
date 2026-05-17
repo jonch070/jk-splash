7f030c9bd112f72399515f7ac98486c665b10164
jonch070 <jonathan.kawchuk@gmail.com>
2026-05-17 14:25:00 -0600

    deploy: include fonts for site-wide Dax usage

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "/Users/jonathankawchuk/Documents/Projects/In Progress/Web Design"
-git add index.html graph.md force-fdeb.html horizontal-bundle.html previews/
+git add index.html graph.md force-fdeb.html horizontal-bundle.html edge-bundle.html previews/ fonts/
 git commit -m "deploy $(date '+%Y-%m-%d %H:%M')"
 git push origin master
 echo "Done. Live in ~60s at https://jonch070.github.io/jk-splash/"
